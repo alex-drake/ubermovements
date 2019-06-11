091069415489a9df13efd88a28364b7a9c1ec43a
Alex <aldrake87@gmail.com>
2019-06-11 08:40:44 +0100

    Updated readme and python script

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Uber Movement data meets OpenStreetMap
 
 ## Things you'll need
 - pandas, numpy, osmnx, geopandas
-- Uber Movements Segments to OSM Ways lookup and Speeds data for your area: [https://movement.uber.com/cities/<your city>/downloads/speeds?lang=en-US&tp[y]=<year>&tp[q]=<quarter>](https://movement.uber.com/cities/<your city>/downloads/speeds?lang=en-US&tp[y]=<year>&tp[q]=<quarter>)
+- Uber Movements Segments to OSM Ways lookup and Speeds data for your area: [https://movement.uber.com/cities/<your city>/downloads/speeds?lang=en-US&tp[y]=<year>&tp[q]=<quarter>](https://movement.uber.com/cities/london/downloads/speeds?lang=en-US&tp[y]=<2018&tp[q]=4)
 - somewhere to store the Uber data (for querying) - I used an MS Azure SQL database
 - a shapefile/geojson of your city (for getting the right ways/nodes from OpenStreetMap) - I used London but it's too big to upload to GitHub.
 
@@ -12,4 +12,5 @@ This work assumes the speed limits detailed in OpenStreetMap are correct and up 
 
 Some ways have multiple speed limits listed. I've taken the largest limit provided (quick and dirty) but you could try to compare it to OS OpenRoads or similar to reach a consensus rather than taking a guess.
 
-<img src="https://github.com/alex-drake/ubermovements/blob/master/plots/centrallondonspeeds.png?raw=true" width="352">
+<img src="https://github.com/alex-drake/ubermovements/blob/master/plots/londonspeeds.png?raw=true" width="650">
+<img src="https://github.com/alex-drake/ubermovements/blob/master/plots/centrallondonspeeds.png?raw=true" width="650">
--- summaries.py
+++ summaries.py
@@ -9,9 +9,7 @@ import pandas as pd
 import numpy as np
 import os
 import osmnx as ox
-#import pickle
 import geopandas as gpd
-#from shapely.geometry import Polygon, shape
 
 file_list = set(os.listdir('./data/'))
 
@@ -20,8 +18,10 @@ if 'uberspeeds.graphml' in file_list:
     london_graph = ox.save_load.load_graphml('uberspeeds.graphml',folder='data')
     
 else:
-    
+    # import segments look-up, available from Uber Movements
     segments = pd.read_csv('./data/segments_lookup.csv')
+
+    # import output from sql query (could have queried direct into Python, but cba)
     uber_df = pd.read_csv('./data/outputs.csv')
 
     uber_df = uber_df.merge(segments, how='left', on='segment_id')
@@ -29,15 +29,13 @@ else:
     uber_summary = uber_df[['osm_way_id','sum_mn_spd','sum_std_spd']].groupby(['osm_way_id'], as_index=False).sum()
     uber_summary['speed'] = uber_summary['sum_mn_spd'] / uber_summary['sum_std_spd']
     
+    # check to see if London outline exists, if not import shapefile and use (not in repo, and use your own city!)
     if 'london.graphml' in file_list:
         london_graph = ox.save_load.load_graphml('london.graphml',folder='data')
     else:
         london_outline = gpd.read_file('../gis/london.shp')
         london_graph = ox.graph_from_polygon(london_outline.geometry.to_dict()[0], network_type='drive')
-        #london_graph = ox.graph_from_place('London, UK', which_result=2, network_type='drive')
         ox.save_load.save_graphml(london_graph, filename='london.graphml', folder='data')
-    
-    #list(london_graph.edges(data=True))[0]
     
     for from_node, to_node, edge in london_graph.edges(data=True):
         
@@ -75,46 +73,30 @@ else:
             speed_lim = pd.to_numeric(speeds.split(' ')[0], errors=np.nan, downcast='float')
         
         # add to edge data
+        # calculate speed-to-limit ratio
         diff = uber_speed / speed_lim
+        # assign linewidth for different ratios
         lw = 2 if 1 <= diff < 1.25 else 3 if diff >= 1.25 else 1
+        # assign different line colour for different ratios
         colour = '#ffbf00' if 1 <= diff < 1.25 else '#99cc99' if diff < 1 else '#f60404' if diff >= 1.25 else '#c6c7be'
         
+        # add back into graph
         london_graph[from_node][to_node][0]['uberspeedmph'] = uber_speed
         london_graph[from_node][to_node][0]['speedint'] = speed_lim
         london_graph[from_node][to_node][0]['diff'] = uber_speed / speed_lim
         london_graph[from_node][to_node][0]['colours'] = colour
         london_graph[from_node][to_node][0]['lw'] = lw
-
-#    # extract nodes/edges as geodataframe so we can do some other fun stuff to it
-#    nodes, edges = ox.graph_to_gdfs(london_graph)
-#    
-#    # create a diff measure ie differences of speed vs speed limit
-#    edges['speedint'] = edges['maxspeed'].apply(lambda x: pd.to_numeric(x.split(' ')[0], errors='ignore', downcast='float') if type(x) == str else pd.to_numeric(x[0].split(' ')[0], errors='ignore', downcast='float') if type(x) == list else np.nan)
-#    edges['diff'] = edges['uberspeedmph']/edges['speedint']
-#    edges['diff'][edges['diff'].isna()] = 0
-#    edges['colours'] = '#c6c7be'
-#    edges['colours'][edges['diff'] < 1.25] = '#ffbf00'
-#    edges['colours'][edges['diff'] < 1] = '#99cc99'
-#    edges['colours'][edges['diff'] >= 1.25] = '#f60404'
-#    edges['lw'] = 1
-#    edges['lw'][edges['diff'] < 1.25] = 2
-#    edges['lw'][edges['diff'] >= 1.25] = 3
-#    
-#    # convert back to a graph network
-#    uber_speeds = ox.save_load.gdfs_to_graph(nodes,edges)
-    #list(london_graph2.edges(data=True))[0]
     
     # save this for use later
     ox.save_load.save_graphml(london_graph, filename='uberspeeds.graphml', folder='data')
 
 # copy and run this in the console to get a pop-up window:- %matplotlib qt5
+# you can then save specific plots from there. If you know where to look, you could just set plot limits instead (again, cba)
 # set edge colors
 nodes, edges = ox.graph_to_gdfs(london_graph)
 edges.oneway = edges.oneway.apply(lambda x: int(x == 'true'))
 edges_lite = edges[['colours','diff','geometry','speedint','u','v','uberspeedmph']]
-edges_lite.to_file('./data/uberspeeds.geojson', driver='GeoJSON')
-#ox.save_load.save_gdf_shapefile(edges, filename='uberspeeds.shp',folder='data')
-
+edges_lite.to_file('./data/uberspeeds.geojson', driver='GeoJSON') # save for later
 
 ec = pd.DataFrame([data for u, v, data in london_graph.edges(data=True)])
 ec['colours'][ec.colours.isna()] = '#c6c7be'
@@ -122,19 +104,3 @@ ec['lw'][ec.lw.isna()] = 1
 
 fig, ax = ox.plot_graph(london_graph, edge_color=ec['colours'], edge_linewidth=ec['lw'], 
                         node_size=1, node_zorder=1, fig_height=16, dpi=420)
-# try this - could try converting to folium, or alternatively dump out as geojson and load in elsewhere?
-#from IPython.display import IFrame
-#graph_map = ox.plot_graph_folium(london_graph, popup_attribute='uberspeedmph', edge_color=ec['colours'], 
-#                                 edge_width=ec['lw'])
-#filepath = 'data/uberspeeds.html'
-#graph_map.save(filepath)
-#IFrame(filepath, width=600, height=500)
-
-## ============= ##
-## TRASH, maybe? ##
-## ============= ##
-
-#pickle.dump(fig, open('./plots/uberfigure.fig.pickle', 'wb'))
-#
-#figx = pickle.load(open('./plots/uberfigure.fig.pickle', 'rb'))
-#figx.show()
